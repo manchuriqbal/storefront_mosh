a8e283cff92e09614c936cb23b07598d2999c366
manchur <manchur.fiverr@gmail.com>
2023-04-18 21:09:15 +0600

    i add some changes

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
-
-
+from django.db.models import Q, F
+from store.models import Product, OrderItem, Order
+1
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    
+    queryset= Order.objects.select_related("customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
+
+    return render(request, 'hello.html', {'name': 'Mosh', 'orders' : list(queryset)})
+  
