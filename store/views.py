97e032597b444d54247daa90206cd0c933680405
manchur <manchur.fiverr@gmail.com>
2023-05-20 12:54:42 +0600

    Deserializing Objects

--- store/views.py
+++ store/views.py
@@ -8,11 +8,16 @@ from .serializer import ProductSerializer
 
 # Create your views here.
 
-@api_view()
+@api_view(["GET", "POST"])
 def product_list(request):
-    queryset = Product.objects.select_related("collection").all()
-    serializer= ProductSerializer(queryset, many=True, context={'request': request})
-    return Response(serializer.data)
+    if request.method == "GET":
+
+        queryset = Product.objects.select_related("collection").all()
+        serializer= ProductSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        return Response("Ok")
 
 @api_view()
 def product_details(request, id ):
