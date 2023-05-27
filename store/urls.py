03a05a3c26d31bc43184ae949eedc715aa770f0a
manchur <manchur.fiverr@gmail.com>
2023-05-27 18:27:28 +0600

    add class collection list and collection deatils

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('product/', views.ProductList.as_view()),
     path('product/<int:id>/', views.ProductDetails.as_view()),
-    path('collection/', views.collection_list),
-    path('collection/<int:pk>/', views.collection_deatil, name="collection_deatil")
+    path('collection/', views.CollectionList.as_view()),
+    path('collection/<int:pk>/', views.CollectionDeatils.as_view(), name="collection_deatil")
 ]
--- store/views.py
+++ store/views.py
@@ -44,33 +44,38 @@ class ProductDetails(APIView):
             return Response({"error" : "it can't be deleted, beacuse this is associate with OrderItem"},status=status.HTTP_404_NOT_FOUND)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(["GET", "POST"])
-def collection_list(request):
-    if request.method == "GET":
+    
+class CollectionList(APIView):
+    def get(self, request):
         queryset= Collection.objects.annotate(product_count=Count("products")).all()
         serializer= CollectionSerializer(queryset, many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
+    def post(self, request):
         serializer=CollectionSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    
 
-@api_view(["GET", "PUT", "DELETE"])
-def collection_deatil(request, pk):
-    collection= get_object_or_404(
+class CollectionDeatils(APIView):
+    def get(self, request, pk):
+        collection= get_object_or_404(
         Collection.objects.annotate(
         product_count=Count("products")), pk= pk)
-    if request.method == "GET":
         serializer= CollectionSerializer(collection)
         return Response(serializer.data)
-    elif request.method == "PUT":
+    def put(self, request, pk):
+        collection= get_object_or_404(
+        Collection.objects.annotate(
+        product_count=Count("products")), pk= pk)
         serializer=CollectionSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(Collection, status=status.HTTP_202_ACCEPTED)
-    elif request.method == "DELETE":
+    def delete(self, request, pk):
+        collection= get_object_or_404(
+        Collection.objects.annotate(
+        product_count=Count("products")), pk= pk)
         if collection.products.count() > 0:
             return Response({"error" : "it can't be deleted, beacuse this collection have Products "},status=status.HTTP_404_NOT_FOUND)
         collection.delete()
