919f58c3b042faf6aba9d29eec276ea9ef3d5616
manchur <manchur.fiverr@gmail.com>
2023-06-02 20:33:11 +0600

    add search in productlist

--- store/views.py
+++ store/views.py
@@ -1,15 +1,9 @@
-from typing import Any
-from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
 from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
-from rest_framework.decorators import api_view
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
 from .models import Product, Collection, OrderItem, Review
 from .serializer import ProductSerializer, CollectionSerializer, ReviewSerializer
 from .filters import ProductFilter
@@ -19,9 +13,9 @@ class ProductViewSet(ModelViewSet):
     
     queryset= Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class= ProductFilter
-
+    search_fields= ["title", "description"]
  
     def get_serializer_context(self): 
         return {'request': self.request}
