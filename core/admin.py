9c28d811192e853f8ba249ed55bc118595337c8a
manchur <manchur.fiverr@gmail.com>
2023-06-13 02:07:19 +0600

    extending the user model

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BasedUserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from store.admin import ProductAdmin
 from store.models import Product
 from tags.models import TaggedItem
+from .models import User
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(BasedUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("first_name", "last_name","username","email" , "password1", "password2"),
+            },
+        ),
+    )
 
 class TagInline(GenericTabularInline):
     model=TaggedItem
