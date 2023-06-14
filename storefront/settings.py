bc7b0f93ae617dc9bd5c51334614da3c0899d3c1
manchur <manchur.fiverr@gmail.com>
2023-06-14 23:32:24 +0600

    add json web token

--- storefront/settings.py
+++ storefront/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -162,4 +163,7 @@ DJOSER = {
     "SERIALIZERS" : {
         "user_create" : "core.serializer.UserCreateSerializer"
     }
+}
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
 }
