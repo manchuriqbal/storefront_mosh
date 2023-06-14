2f38dd201c99f2cdad35795884f37ca70a1d4b64
manchur <manchur.fiverr@gmail.com>
2023-06-14 13:41:18 +0600

    user permissions

--- store/models.py
+++ store/models.py
@@ -89,6 +89,11 @@ class Order(models.Model):
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+    class Meta:
+        permissions = [
+            ("can_cancel", "Can cancel Order")
+        ]
+
    
 
 
