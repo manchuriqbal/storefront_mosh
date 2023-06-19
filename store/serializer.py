a5b705f9afb3147f0547a1b99a956755f52d8921
manchur <manchur.fiverr@gmail.com>
2023-06-19 19:24:34 +0600

    returing the created order

--- store/serializer.py
+++ store/serializer.py
@@ -139,3 +139,6 @@ class CreateOrderSerializer(serializers.Serializer):
             OrderItem.objects.bulk_create(order_item)
 
             Cart.objects.filter(pk=cart_id).delete()
+            
+
+            return order 
