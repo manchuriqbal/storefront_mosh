b851f37ae3c3a684ce6ffc4e60caa083177ad019
manchur <manchur.fiverr@gmail.com>
2023-06-25 01:17:50 +0600

    reteving data

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
 from django.db import transaction, connection
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, Collection, Order, OrderItem
 from tags.models import TaggedItem
 
 def say_hello(request):
 
-    queryset = Product.objects.all()
-    for product in queryset:
-        print(product)
+    queryset = Product.objects.filter(pk=0).exists()
+    
+    
+    
 
     return render(request, 'hello.html', {'name': 'Mosh'})
   
