9c1aeae93d3c6ed5c0f74b8fe8ae6ea14a2ed186
manchur <manchur.fiverr@gmail.com>
2023-06-25 10:20:54 +0600

    task complete

--- playground/views.py
+++ playground/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
 from django.db import transaction, connection
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, Collection, Order, OrderItem
+from store.models import Product, Collection, Order, OrderItem, Customer
 from tags.models import TaggedItem
 
 def say_hello(request):
 
-    queryset = Product.objects.filter(unit_price__gt=20)
-    
-    
-    
 
+    queryset = OrderItem.objects.filter(product__collection__id=3)
+    
     return render(request, 'hello.html', {'name': 'Mosh', "produts" : queryset})
   
