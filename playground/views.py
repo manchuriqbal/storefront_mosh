7d7b4d69acd1f20b18ca2c58a7fb4fa53b0ad2fc
manchur <manchur.fiverr@gmail.com>
2023-06-28 02:10:45 +0600

    annotate

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
 from django.db import transaction, connection
-from django.db.models import Q, F
+from django.db.models import Q, F, Value
 from django.db.models.aggregates import Count, Min, Max, Avg
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, Collection, Order, OrderItem, Customer
@@ -9,7 +9,7 @@ from tags.models import TaggedItem
 
 def say_hello(request):
 
-    queryset = Product.objects.filter(collection_id=3).aggregate(Minimum= Min("unit_price"), Maximum= Max("unit_price"),Avgarage= Avg("unit_price"),)
+    queryset = Product.objects.annotate(new=F("id"))
 
     return render(request, 'hello.html', {'name': 'Mosh', "produts" : queryset})
      
