859e3243512857fb89b505d0af3bc108ef540a88
manchur <manchur.fiverr@gmail.com>
2023-06-28 15:28:17 +0600

    groping data

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
 from django.db import transaction, connection
-from django.db.models import Q, F, Value, Func
+from django.db.models import Q, F, Value, Func, Count
 from django.db.models.functions import Concat
 from django.db.models.aggregates import Count, Min, Max, Avg
 from django.core.exceptions import ObjectDoesNotExist
@@ -10,7 +10,7 @@ from tags.models import TaggedItem
 
 def say_hello(request):
 
-    queryset = Customer.objects.select_related("user").annotate(full_name=Concat("user__first_name", Value(" "), "user__last_name"))
+    queryset = Customer.objects.annotate(order_count=Count("order"))
 
     return render(request, 'hello.html', {'name': 'Mosh', "produts" : queryset})
      
