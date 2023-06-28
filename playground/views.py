f67c98d864f0e3b9e38aa904ca936a98fd6c40d1
manchur <manchur.fiverr@gmail.com>
2023-06-28 15:47:52 +0600

    expression wrappers

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
 from django.db import transaction, connection
-from django.db.models import Q, F, Value, Func, Count
+from django.db.models import Q, F, Value, Func, Count, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat
 from django.db.models.aggregates import Count, Min, Max, Avg
 from django.core.exceptions import ObjectDoesNotExist
@@ -9,8 +9,11 @@ from store.models import Product, Collection, Order, OrderItem, Customer
 from tags.models import TaggedItem
 
 def say_hello(request):
+    discounted_price = ExpressionWrapper( F("unit_price") * 0.8, output_field=DecimalField())
 
-    queryset = Customer.objects.annotate(order_count=Count("order"))
+    queryset = Product.objects.annotate(
+        discounted_price = discounted_price
+    )
 
-    return render(request, 'hello.html', {'name': 'Mosh', "produts" : queryset})
+    return render(request, 'hello.html', {'name': 'Mosh', "produts" : list(queryset)})
      
