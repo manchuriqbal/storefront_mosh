9e6f40619b2dda21929c9335e0bf6cabdad1e0e1
manchur <manchur.fiverr@gmail.com>
2023-07-04 22:41:02 +0600

    create object

--- playground/views.py
+++ playground/views.py
@@ -10,14 +10,11 @@ from tags.models import TaggedItem
 
 def say_hello(request):
     
-    content_type = ContentType.objects.get_for_model(Product)
+    collection = Collection()
+    collection.title = "Gaget Items"
+    collection.featured_product = Product(pk=1)
+    collection.save()
+    
 
-    queryset = TaggedItem.objects \
-    .select_related('tag') \
-    .filter(
-        content_type = content_type,
-        object_id = 1
-    )
-
-    return render(request, 'hello.html', {'name': 'Mosh', "produts" : list(queryset)})
+    return render(request, 'hello.html', {'name': 'Mosh'})
      
